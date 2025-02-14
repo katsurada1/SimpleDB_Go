5e8caf54b35633dda4543b734956ddcb382450c8
kasturada1 <kazuki1.katsurada@gmail.com>
2025-02-14 21:54:51 +0900

    Add the log manager to simpledb

--- simpledb/server/simpledb.go
+++ simpledb/server/simpledb.go
@@ -1,19 +1,32 @@
 package server
 
 import (
-	"simpledb/file"
+	"fmt"
+	"simpledb_go/simpledb/file"
+	"simpledb_go/simpledb/log"
 )
 
 type SimpleDB struct {
 	fm *file.FileMgr
+	lm *log.LogMgr
 }
 
 func NewSimpleDB(dirname string, blockSize, buffSize int) *SimpleDB {
+	fm := file.NewFileMgr(dirname, blockSize)
+	lm := log.NewLogMgr(fm, "logfile")
+	if lm == nil {
+			panic("NewSimpleDB: LogMgr is nil! Initialization failed.")
+	}
 	return &SimpleDB{
-		fm: file.NewFileMgr(dirname, blockSize),
+			fm: fm,
+			lm: lm,
 	}
 }
 
 func (db *SimpleDB) FileMgr() *file.FileMgr {
 	return db.fm
+}
+
+func (db *SimpleDB) LogMgr() *log.LogMgr {
+	return db.lm
 }
