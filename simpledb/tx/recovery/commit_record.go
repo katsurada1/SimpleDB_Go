0e926c280b9d5ad04131f5a0b983afc39bc97412
kasturada1 <kazuki1.katsurada@gmail.com>
2025-02-28 15:58:19 +0900

    Add commit record function

---
+++ simpledb/tx/recovery/commit_record.go
@@ -0,0 +1,44 @@
+package recovery
+
+import (
+	"fmt"
+
+	"simpledb_go/simpledb/file"
+	"simpledb_go/simpledb/log"
+)
+
+// CommitRecord は COMMIT ログレコードを表します。
+type CommitRecord struct {
+	txnum int
+}
+
+func NewCommitRecordFromPage(p *file.Page) *CommitRecord {
+	tpos := 4
+	return &CommitRecord{
+		txnum: p.GetInt(tpos),
+	}
+}
+
+func (cr *CommitRecord) Op() int {
+	return COMMIT
+}
+
+func (cr *CommitRecord) TxNumber() int {
+	return cr.txnum
+}
+
+func (cr *CommitRecord) Undo(tx Tx) {
+	// COMMIT レコードは undo を行わない
+}
+
+func (cr *CommitRecord) String() string {
+	return fmt.Sprintf("<COMMIT %d>", cr.txnum)
+}
+
+func WriteCommitRecord(lm *log.LogMgr, txnum int) int {
+	rec := make([]byte, 2*4)
+	p := file.NewPageFromBytes(rec)
+	p.SetInt(0, COMMIT)
+	p.SetInt(4, txnum)
+	return lm.Append(rec)
+}
