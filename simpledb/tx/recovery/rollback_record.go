3ac1813e72ac3ee3a47bf2083bd8651e5fd48fef
kasturada1 <kazuki1.katsurada@gmail.com>
2025-02-28 15:58:51 +0900

    Add rollback record function

---
+++ simpledb/tx/recovery/rollback_record.go
@@ -0,0 +1,44 @@
+package recovery
+
+import (
+	"fmt"
+
+	"simpledb_go/simpledb/file"
+	"simpledb_go/simpledb/log"
+)
+
+// RollbackRecord は ROLLBACK ログレコードを表します。
+type RollbackRecord struct {
+	txnum int
+}
+
+func NewRollbackRecordFromPage(p *file.Page) *RollbackRecord {
+	tpos := 4
+	return &RollbackRecord{
+		txnum: p.GetInt(tpos),
+	}
+}
+
+func (rr *RollbackRecord) Op() int {
+	return ROLLBACK
+}
+
+func (rr *RollbackRecord) TxNumber() int {
+	return rr.txnum
+}
+
+func (rr *RollbackRecord) Undo(tx Tx) {
+	// ROLLBACK レコードは undo を行わない
+}
+
+func (rr *RollbackRecord) String() string {
+	return fmt.Sprintf("<ROLLBACK %d>", rr.txnum)
+}
+
+func WriteRollbackRecord(lm *log.LogMgr, txnum int) int {
+	rec := make([]byte, 2*4)
+	p := file.NewPageFromBytes(rec)
+	p.SetInt(0, ROLLBACK)
+	p.SetInt(4, txnum)
+	return lm.Append(rec)
+}
